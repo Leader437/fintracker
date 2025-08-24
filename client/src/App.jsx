56545eb2996d1297919f945b17e3c27fc4f3866d
Saif <raosaif786pak@gmail.com>
2025-08-24 23:49:14 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-custom-faded-green">Welcome to Fintracker</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+      },
+      colors: {
+        primarycolor: "#FFFFFF",
+      },
+      screens: {
+        'sm': '480px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1200px',
+        '2xl': '1440px',
+      },
+    },
+  },
+  plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        ".container": {
+          width: '100%',
+          maxWidth: '1200px',
+          paddingRight: '16px',
+          paddingLeft: '16px',
+          [`@media (min-width: ${theme("screens.md")})`]: {
+            paddingRight: "32px",
+            paddingLeft: "32px",
+          },
+          [`@media (min-width: ${theme("screens.lg")})`]: {
+            paddingRight: "52px",
+            paddingLeft: "52px",
+          },
+        },
+      });
+    },
+  ],
+}
