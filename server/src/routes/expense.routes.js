7eb975d78882a82182a32944c53f3e7a0b9df98e
Saif <raosaif786pak@gmail.com>
2025-12-27 14:19:25 +0500

    Expense Api's tested successfuly

--- server/src/routes/expense.routes.js
+++ server/src/routes/expense.routes.js
@@ -10,7 +10,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route('/add-expense').post(verifyJWT, createExpense);
-router.route('/get-expenses').get(verifyJWT, getExpense);
+router.route('/get-expense').get(verifyJWT, getExpense);
 router.route('/update-expense/:id').put(verifyJWT, updateExpense);
 router.route('/delete-expense/:id').delete(verifyJWT, deleteExpense);
 
