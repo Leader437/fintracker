47895cf48d5758905a94e6ffc39be5fe064be0b1
Saif <raosaif786pak@gmail.com>
2025-12-29 16:22:18 +0500

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,5 +10,8 @@ export default defineConfig({
     proxy: {             // advantage of using proxy is that in development we don't have to hardcode the backend server url in our frontend code, when we use proxy, we can just make request to our own frontend server and it will forward the request to the backend server, so now the browser will think that it is making request to the same server and it won't block the request and cause CORS error, in this way we can avoid CORS error without any whitelisting of frontend url or any proper CORS setup in backend server during development, we obviously need to do proper CORS setup in production
       '/api': 'http://localhost:3000'              // now wherever the frontend makes request to a route starting with /api, it will be forwarded to the mentioned backend server i.e. http://localhost:3000
     }
+  },
+    preview: {
+    allowedHosts: ['.onrender.com']
   }
 })
