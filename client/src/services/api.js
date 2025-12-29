a0ff57d2b183779fe11db7fd2d519f536bf397c6
Saif <raosaif786pak@gmail.com>
2025-12-29 17:37:22 +0500

    testing with api url in code

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = "https://fintracker-server-twcf.onrender.com/api/v1";
 
 // Create an axios instance
 const api = axios.create({
